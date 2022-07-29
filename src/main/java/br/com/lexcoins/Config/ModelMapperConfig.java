bc8a8d80f5771db621a94afc25d7e4b064f97810
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2022-07-29 19:08:19 -0300

    atualizando alguns models, criando repository, service e controller de person, criando estrutura de swagger e de banco

---
+++ src/main/java/br/com/lexcoins/Config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package br.com.lexcoins.Config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper ModelMapper(){
+        var modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+}
--- src/main/java/br/com/lexcoins/enums/Crypto.java
+++
@@ -1,4 +0,0 @@
-package br.com.lexcoins.enums;
-
-public enum Crypto {
-}
