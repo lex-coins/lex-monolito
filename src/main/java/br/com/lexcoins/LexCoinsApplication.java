b8016e88367d1dcb59778a2981275825533e6e8a
Beatriz Santos <beatriz_santos0207@hotmail.com>
2022-07-29 15:45:44 -0300

    Criação estrutura inicial projeto

---
+++ src/main/java/br/com/lexcoins/LexCoinsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lexcoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexCoinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lexcoins/LexCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lexcoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
