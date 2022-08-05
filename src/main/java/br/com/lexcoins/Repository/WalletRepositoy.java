7429cb31879d89dcaf287558ebb3e3c3fc8e77a0
RitaFer <rialf.ferreira@gmail.com>
2022-08-04 22:02:28 -0300

    add validation spring-boot

--- src/main/java/br/com/lexcoins/Repository/WalletRepositoy.java
+++ src/main/java/br/com/lexcoins/Repository/WalletRepositoy.java
@@ -1,7 +1,10 @@
-package br.com.lexcoins.repository;//package repository;
+package br.com.lexcoins.Repository;//package repository;
 
 import br.com.lexcoins.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletRepositoy extends JpaRepository<Wallet,Long> {
+    List<Wallet> findByPersonId(Long id);
 }
