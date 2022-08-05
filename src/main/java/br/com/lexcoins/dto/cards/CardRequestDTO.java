5d44e447cb86b38cc3aeb4096eeffd23dbaade76
RitaFer <rialf.ferreira@gmail.com>
2022-08-04 22:03:01 -0300

    Create CardRequestDTO.java

---
+++ src/main/java/br/com/lexcoins/dto/cards/CardRequestDTO.java
@@ -0,0 +1,30 @@
+package br.com.lexcoins.dto.cards;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CardRequestDTO {
+    @NotEmpty
+    private String network;
+    @NotEmpty
+    @Size(max = 16)
+    @CreditCardNumber
+    private String number;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    @Size(max = 3)
+    private String cvv;
+    @NotNull
+    @JsonFormat(pattern = "MM/yyyy")
+    private String expiration;
+}
